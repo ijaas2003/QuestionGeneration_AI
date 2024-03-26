33363ef941441f01ed81a615c575bd3b98ea33d5
vino583 <arulvino8@gmail.com>
2024-03-26 15:03:22 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,7 @@ function App() {
     <>
       <div>
         <h1>jhshdfjihsbui huifhuihfbguihu</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        
       </div>
       <h1>Vite + React</h1>
       <div className="card">
