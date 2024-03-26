efaac143c987deec769d7172823e5bba88ab11a4
ijaasM4 <98891153+ijaasM4@users.noreply.github.com>
2024-03-26 19:51:21 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/user/Login'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +8,7 @@ function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route />
+          <Route path='/' element={<Login />}/>
         </Routes>
       </BrowserRouter>
   )
