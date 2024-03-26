c49149da4f541e380b71fd04b3d05af40463c72d
ijaasM4 <98891153+ijaasM4@users.noreply.github.com>
2024-03-26 21:13:45 +0530

    File

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,5 +1,27 @@
-
+import { useState } from "react";
 const Login = () => {
-	
+	const [username, setUser] = useState('');
+	const [Password, setPass] = useState('');
+	console.log(username, Password);
+	const HandleSubmit = () => {
+		fetch('http://localhost:5000/Login', {
+			method:"POST",
+			headers:{
+				"content-Type":"application/json"
+			},
+			body:JSON.stringify({username, Password})
+		}).then(res => {
+			return res.json()
+		}).then(res => {
+			console.log(res.message);
+		})
+	}
+	return(
+		<>
+			<input onChange={(e) => setUser(e.target.value)}/>
+			<input onChange={(e) => setPass(e.target.value)}/>
+			<button onClick={HandleSubmit}>Submit</button>
+		</>	
+	)
 }
 export default Login;
