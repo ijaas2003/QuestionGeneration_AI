50ffff801d3080f1ab75f9318f73c89e0b32a844
ijaasM4 <98891153+ijaasM4@users.noreply.github.com>
2024-03-26 15:23:52 +0530

    y

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Welcome To You</h1>
+        <h1>Welcome</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
