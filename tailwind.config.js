76d2fb39c67d734c3434e87e1679a36363a118db
ijaasM4 <98891153+ijaasM4@users.noreply.github.com>
2024-03-28 12:25:56 +0530

    okk

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,13 @@ export default {
       colors:{
         LightRed:"#de5d5b",
         Purple:"#9451e0",
-        LightGreen:"#52e3b3"
+        LightGreen:"#71e1b5"
       },
       backgroundImage: theme => ({
         'home': "url('/src/assets/Home.jpg')",
         'homie': "url('/src/assets/homie.png')",
-        'back':"url('/src/assets/back.png')"
+        'back':"url('/src/assets/back.png')",
+        'back1':"url('/src/assets/BACK.jpg')"
       }),
     },
   },
