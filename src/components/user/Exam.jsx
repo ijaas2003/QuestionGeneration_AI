0182e711a08a9a51a4eaf73d2fe4a8e1d2e04340
vino583 <arulvino8@gmail.com>
2024-04-04 11:47:35 +0530

    ok

--- src/components/user/Exam.jsx
+++ src/components/user/Exam.jsx
@@ -3,8 +3,16 @@ const Exam = () => {
 	
 	return (
 		<section>
-			<div>
-				<h1>This is exam </h1>
+			<div className="main bg-white h-[100vh]">
+				<div className="m1 h-[100vh] flex justify-center items-center">
+					<div className="m2 bg-slate-300 h-[50%] w-[50%] justify-center rounded-3xl  flex relative">
+						<div>
+						<h1 className="he text-[25px] text-neutral-700 font-bold text-center ">Test  </h1>
+						<label className="lab   mt-[30px] font-bold">Starting Time</label><input type="text" placeholder="Enter the Stating Time"  className="inp h-10 w-full  mt-[30px] font-bold" />
+						</div>
+						
+					</div>
+				</div>
 			</div>
 		</section>
 	)
